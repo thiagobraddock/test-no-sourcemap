4d477fe811864de5aa28f12b2c4b120681597340
thiago braddock <braddock01@hotmail.com>
2022-01-27 18:30:53 -0300

    Fix add api key to .env

--- src/components/Teste.js
+++ src/components/Teste.js
@@ -3,8 +3,8 @@ import React from 'react';
 function Teste() {
   const teste = process.env.REACT_APP_API_KEY;
 
-  if (teste) {
-    return <p>API KEY ESTA PRESENTE</p>;
+  if (teste != null) {
+    return <p>API KEY ESTA PRESENTE {teste}</p>;
   }
   return <div> API KEY NOT FOUND</div>;
 }
